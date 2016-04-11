a329df4563b04f14560b86e8742478eb8f245098
naridas <gregsparta@gmail.com>
2016-04-11 15:26:24 +0100

    step 5 complete config spec

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,8 @@
+describe DockingStation do #class not string.
+	#it 'release_bike when person needs bike' do
+ 	#	expect(docking-station(true)).to eq 'bike released'
+	#end
+	#it 'release_bike when person needs bike' do
+ 	#	expect(docking-station(false)).to eq 'bike not released'
+	#end	
+end
