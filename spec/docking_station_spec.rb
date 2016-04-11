0a04141added95b944ead916978dd399f67a592b
ChrisKummelstedt <chris.kummelstedt@gmail.com>
2016-04-11 16:38:01 +0200

    6 complete

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,10 @@
+require 'docking_station'
+
 describe DockingStation do #class not string.
 	#it 'release_bike when person needs bike' do
  	#	expect(docking-station(true)).to eq 'bike released'
 	#end
 	#it 'release_bike when person needs bike' do
  	#	expect(docking-station(false)).to eq 'bike not released'
-	#end	
+	#end
 end
