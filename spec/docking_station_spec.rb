326444e1cd2068a79cffe627cc928f56063ab873
naridas <gregsparta@gmail.com>
2016-04-11 15:58:43 +0100

    ch8 finished added metod to class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,10 @@
 require 'docking_station'
 
 describe DockingStation do #class not string.
-	#it 'release_bike when person needs bike' do
- 	#	expect(docking-station(true)).to eq 'bike released'
-	#end
-	#it 'release_bike when person needs bike' do
- 	#	expect(docking-station(false)).to eq 'bike not released'
-	#end
+	#it 'responds to release_bike' do
+    	#	expect(subject).to respond_to :release_bike
+  	#end	
+
+	it { is_expected.to respond_to :release_bike }
+
 end
