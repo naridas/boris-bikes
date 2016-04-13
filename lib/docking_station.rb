adbdc8012713892a9e7a58a7bf5bc5641a13ca16
naridas <gregsparta@gmail.com>
2016-04-13 13:37:19 +0100

    complete aprt13 walk

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,10 +3,12 @@ require_relative 'bike'
 class DockingStation
 	attr_reader :bike
 	def release_bike
+		raise 'No bikes available' unless @bike
 		Bike.new
 	end
 
   	def dock(bike)
+		raise 'Docking station full' if @bike
 		@bike = bike
   	end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -18,5 +18,24 @@ describe DockingStation do
     		# We want to return the bike we dock
     		expect(subject.dock(bike)).to eq bike
   	end
+	describe '#release_bike' do
+    		it 'releases a bike' do
+      			bike = Bike.new
+      			subject.dock(bike)
+      			# we want to release the bike we docked
+      			expect(subject.release_bike).to eq bike
+    		end
+    		it 'raises an error when there are no bikes available' do
+     		  #Let's not dock a bike first:
+      		  #remember subject == DockingStation.new
+      		  expect { subject.release_bike }.to raise_error 'No bikes available'
+    		end
+  	end
+  	describe '#dock' do
+    		it 'raises an error when full' do
+      		  subject.dock(Bike.new)
+      		  expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    		end
+  	end
 
 end
